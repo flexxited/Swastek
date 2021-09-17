8e957699c5bb4c74cbc875c37661aa31e9c4b9fd
Dell <tushargoel.tg@gmail.com>
2021-09-17 14:19:21 +0530

    main change

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,14 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
