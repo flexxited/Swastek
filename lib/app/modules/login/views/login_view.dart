b249683cd4622a950a582b43463bc0d9bfc92101
Dell <tushargoel.tg@gmail.com>
2021-09-18 12:23:31 +0530

    update

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -5,6 +5,8 @@ import 'package:get/get.dart';
 import '../controllers/login_controller.dart';
 import 'widgets/reusable_account_screen.dart';
 
+// udpdate
+
 class LoginView extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
